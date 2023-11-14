cd945b3a23c85500c999f631915a0021229104a2
Saquib Ahsan <saquibahsan73@gmail.com>
2023-11-14 16:54:47 +0530

    build layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
